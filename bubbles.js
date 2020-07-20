f7a78b0e37f51bf8feb0d84e64597d03e02ecda7
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-20 14:58:07 -0400

    html page with canvas and bubble object

---
+++ bubbles.js
@@ -0,0 +1,29 @@
+let bubble;
+
+
+function draw() {
+    background(0);
+    bubble.move();
+    bubble.show();
+}
+
+class Bubble {
+    constructor() {
+        this.x = 200;
+        this.y = 150;
+    }
+    move() {
+        this.x = this.x + random(-5, 5);
+        this.y = this.y + random(-5, 5);
+    }
+    show() {
+        stroke(255);
+        strokeWeight(4);
+        noFill();
+        ellipse(bubble.x, bubble.y, 24, 24);
+    }
+}
+function setup() {
+    bubble = new Bubble();
+}
+setup();
