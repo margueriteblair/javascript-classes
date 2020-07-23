6d666b773827ee91e32f90be99a2bb538ad199d5
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-23 06:32:54 -0400

    model class that is inherited from car class

--- car.js
+++ car.js
@@ -15,6 +15,18 @@ class Car {
     }
 }
 
+class Model extends Car {
+    constructor(brand, year, owner, mod) {
+        super(brand, year, owner); //the super method refers tothe parent class
+        this.model = mod; //by calling the super() method, we call the parents 
+    } //method and get access to the parents properties and methods;
+    show() {
+        return this.present() + ", it is a " + this.model;
+    }
+}
+
 let deesCar = new Car("BMW", "2016", "Deondra");
-(deesCar.present());
-Car.hello();
+let myCar = new Model("Toyota", "2009", "Margie", "Carola");
+// (deesCar.present());
+// Car.hello();
+console.log(myCar)
