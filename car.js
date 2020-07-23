e957b7cdaa55e7851fd71a2a65cb9602395003a4
Marguerite Blair <marguerite.blair@gmail.com>
2020-07-23 06:18:50 -0400

    Car class now has multiple methods

--- car.js
+++ car.js
@@ -1,9 +1,20 @@
 class Car {
-    constructor(brand, year) {
+    constructor(brand, year, owner) {
         this.carname = brand;
         this.year = year;
+        this.owner = owner;
+    }
+    present() {
+        console.log(`${this.owner} has a ${this.carname}`)
+    }
+    additionalInformation(snippet) {
+        console.log(`${snippet}. Hope you like it!`)
+    }
+    static hello() { //a static method is applied to the class itself, not to the instance
+        console.log("Hello World")
     }
 }
 
-let deesCar = new Car("BMW", "2016");
-console.log(deesCar.carname);
+let deesCar = new Car("BMW", "2016", "Deondra");
+(deesCar.present());
+Car.hello();
